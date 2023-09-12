ffc3aef55122abdc75c4fe1ec6311bceef752aec
kubishechka <scar.zdz@gmail.com>
2023-09-12 22:25:01 +0200

    Make patches version agnostic (DrakiaXYZ's approach)

--- src/CactusPie.FastSearch/SearchTimePatch.cs
+++ src/CactusPie.FastSearch/SearchTimePatch.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Reflection;
 using Aki.Reflection.Patching;
-using TarkovDateTime = GClass1252;
+using Aki.Reflection.Utils;
+using HarmonyLib;
+using System.Linq;
 
 namespace CactusPie.FastSearch
 {
     public class SearchTimePatch : ModulePatch
     {
+        private static Type _searchOperationClass;
+        private static PropertyInfo _tarkovDateTimeNow;
+
         protected override MethodBase GetTargetMethod()
         {
-            MethodInfo method = typeof(GClass2517).GetMethod("GetNextDiscoveryTime", BindingFlags.Static | BindingFlags.Public);
-            return method;
+            Type tarkovDateTime = PatchConstants.EftTypes.Single(
+                x => x.GetProperty("MoscowNow", BindingFlags.Static | BindingFlags.Public) != null && x.GetProperty("Now", BindingFlags.Static | BindingFlags.Public) != null
+            );
+            _tarkovDateTimeNow = AccessTools.Property(tarkovDateTime, "Now");
+
+            _searchOperationClass = PatchConstants.EftTypes.Single(x => x.GetMethod("GetNextDiscoveryTime", BindingFlags.Static | BindingFlags.Public) != null);
+            return _searchOperationClass.GetMethod("GetNextDiscoveryTime", BindingFlags.Static | BindingFlags.Public);
         }
 
         [PatchPostfix]
@@ -23,13 +33,13 @@ namespace CactusPie.FastSearch
 
             float searchTimeMultiplier = FastSearchPlugin.SearchTimeMultiplier.Value;
 
+            DateTime now = (DateTime)_tarkovDateTimeNow.GetValue(null);
             if (searchTimeMultiplier == 0)
             {
-                __result = TarkovDateTime.Now;
+                __result = now;
                 return;
             }
-            
-            __result = TarkovDateTime.Now.AddTicks((long)((__result - TarkovDateTime.Now).Ticks * searchTimeMultiplier));
+            __result = now.AddTicks((long)((__result - now).Ticks * searchTimeMultiplier));
         }
     }
 }
